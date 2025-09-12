a862d1b150012355a63af987c3a673f97c7f0b47
KenulaNimhan <kenulanimhan@gmail.com>
2025-09-12 12:03:11 +0530

    refactor usage of stacks

--- src/main/java/site/visualizer/use/UseStack.java
+++ src/main/java/site/visualizer/use/UseStack.java
@@ -1,63 +1,63 @@
 package site.visualizer.use;
 
+import site.visualizer.StackPerformanceMetrics;
 import site.visualizer.model.Configurable;
 import site.visualizer.model.ThreadType;
 import site.visualizer.model.structure.stack.AdvSyncStack;
 import site.visualizer.model.structure.stack.LockBasedStack;
 import site.visualizer.model.structure.stack.Stack;
+import site.visualizer.model.type.Brick;
 
 import java.util.*;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class UseStack {
     private static final Scanner scan = new Scanner(System.in);
+    private static final Configurable testData = new Configurable();
 
-    private static final AtomicInteger producedCount = new AtomicInteger();
-    private static final AtomicInteger consumedCount = new AtomicInteger();
+    private static final ReentrantLock prodLock = new ReentrantLock();
+    private static final ReentrantLock conLock = new ReentrantLock();
 
-    private static final List<Long> latencyListForSyncStack = Collections.synchronizedList(new ArrayList<>());
-    private static final List<Long> latencyListForLockStack = Collections.synchronizedList(new ArrayList<>());
+    private static final StackPerformanceMetrics<Brick> syncStackMetrics = new StackPerformanceMetrics<>("Synchronised Stack");
+    private static final StackPerformanceMetrics<Brick> lockStackMetrics = new StackPerformanceMetrics<>("Lock Based Stack");
 
 
     public static void main(String[] args) throws InterruptedException {
 
-        // configurable
-        Configurable testData = new Configurable();
-
         // gathering input from user to configure
-        System.out.print("how many bricks are needed to be stacked: ");
+        System.out.print("how many items needed to be produced & consumed: ");
         var toProduce = scan.nextInt();
-
-        var toConsume = Integer.MAX_VALUE;
-        do {
-            System.out.print("how many bricks are needed to be removed: ");
-            toConsume = scan.nextInt();
-            if (toConsume <= toProduce) break;
-            System.out.println("needed amount cannot exceed the available amount ");
-        } while (true);
-
-        System.out.print("how many workers putting to the stack: ");
+        System.out.print("how many producer threads: ");
         var producerCount = scan.nextInt();
-        System.out.print("how many workers taking from the stack: ");
+        System.out.print("how many consumer threads: ");
         var consumerCount = scan.nextInt();
-        System.out.print("what is the max height the stack can be: ");
+        System.out.print("max no. of elements the data structure can hold: ");
         var stackHeight = scan.nextInt();
 
         testData.setCap(stackHeight)
                 .setToProduce(toProduce)
-                .setToConsume(toConsume)
+                .setToConsume(toProduce)
                 .setProducerCount(producerCount)
                 .setConsumerCount(consumerCount);
 
         // test stacks
-        AdvSyncStack<Integer> syncStack = new AdvSyncStack<>(testData.getCap());
-        LockBasedStack<Integer> lockBasedStack = new LockBasedStack<>(testData.getCap());
+        AdvSyncStack<Brick> syncStack = new AdvSyncStack<>(testData.getCap());
+        LockBasedStack<Brick> lockBasedStack = new LockBasedStack<>(testData.getCap());
 
+        runThreads(syncStack, syncStackMetrics);
+        runThreads(lockBasedStack, lockStackMetrics);
+
+        System.out.println(syncStackMetrics);
+        System.out.println(lockStackMetrics);
+
+    }
+
+    private static <T> void runThreads(Stack<T> stack, StackPerformanceMetrics<T> metrics) throws InterruptedException {
         // process for sync stack
         Thread[] producersForSyncStack = getConfiguredThreadPool(
-                ThreadType.PRODUCER, testData, syncStack, latencyListForSyncStack);
+                ThreadType.PRODUCER, stack,metrics);
         Thread[] consumersForSyncStack = getConfiguredThreadPool(
-                ThreadType.CONSUMER, testData, syncStack, latencyListForSyncStack);
+                ThreadType.CONSUMER, stack,metrics);
 
         var start = System.nanoTime();
         // starting
@@ -68,96 +68,62 @@ public class UseStack {
         for (Thread consumer: consumersForSyncStack) consumer.join();
         var end = System.nanoTime();
 
-        var totalTime = (end-start) / 1_000_000;
-        var throughputOfSyncStack = (producedCount.get() +consumedCount.get()) / totalTime;
-        System.out.println("time taken for sync stack: " + totalTime + "ms");
-
-        // process for lock based stack
-        producedCount.set(0);
-        consumedCount.set(0);
-
-        Thread[] producersForLockStack = getConfiguredThreadPool(
-                ThreadType.PRODUCER, testData, lockBasedStack, latencyListForLockStack);
-        Thread[] consumersForLockStack = getConfiguredThreadPool(
-                ThreadType.CONSUMER, testData, lockBasedStack, latencyListForLockStack);
-
-        start = System.nanoTime();
-
-        // starting
-        for (Thread producer: producersForLockStack) producer.start();
-        for (Thread consumer: consumersForLockStack) consumer.start();
-        // joining
-        for (Thread producer: producersForLockStack) producer.join();
-        for (Thread consumer: consumersForLockStack) consumer.join();
-
-        end = System.nanoTime();
-        totalTime = (end-start) / 1_000_000;
-        var throughputOfLockStack = (producedCount.get() +consumedCount.get()) / totalTime;
-        System.out.println("time taken for lock stack: " + totalTime + "ms");
-
-        System.out.println("--CORRECTNESS--");
-        System.out.println("sync stack size: "+syncStack.size());
-        System.out.println("lock stack size: "+lockBasedStack.size());
-
-        System.out.println("--THROUGHPUT--");
-        System.out.println("sync stack: "+throughputOfSyncStack+" ops/ms");
-        System.out.println("lock stack: "+throughputOfLockStack+ " ops/ms");
-
-        Collections.sort(latencyListForSyncStack);
-        Collections.sort(latencyListForLockStack);
-        System.out.println("--LATENCY--");
-        System.out.println("sync - max: "+latencyListForSyncStack.getLast()+" ns");
-        System.out.println("sync - min: "+latencyListForSyncStack.getFirst()+" ns");
-        System.out.println("lock - max: "+latencyListForLockStack.getLast()+" ns");
-        System.out.println("lock - min: "+latencyListForLockStack.getFirst()+" ns");
-
-
+        metrics.setTotalTime((end-start) / 1_000_000);
     }
 
     @SuppressWarnings("unchecked")
-    private static <T> Runnable getProducerRunnable(int quota, Stack<T> stack, List<Long> latencyList) {
+    private static <T> Runnable getProducerRunnable(int quota, Stack<T> stack, StackPerformanceMetrics<T> metrics) {
         return () -> {
             var opsBegin = System.nanoTime();
             for (int i=0; i<quota; i++) {
                 try {
-                    stack.push((T) new Object());
-                    producedCount.getAndIncrement();
-                } catch (IllegalStateException e) {
-                    System.out.println("tried to push when full");
+                    var obj = (T) new Object();
+                    prodLock.lock();
+                    try {
+                        stack.push(obj);
+                        metrics.addToProducedData(obj);
+                    }
+                    finally {
+                        prodLock.unlock();
+                    }
+                    metrics.incrementProducedCount();
                 } catch (InterruptedException e) {
                     System.out.println(e.getMessage());
                 }
             }
             var opsEnd = System.nanoTime();
             var latencyForThread = (opsEnd-opsBegin) / quota;
-            latencyList.add(latencyForThread);
+            metrics.addLatency(latencyForThread);
         };
     }
 
-    private static <T> Runnable getConsumerRunnable(int quota, Stack<T> stack, List<Long> latencyList) {
+    private static <T> Runnable getConsumerRunnable(int quota, Stack<T> stack, StackPerformanceMetrics<T> metrics) {
         return () -> {
             var opsBegin = System.nanoTime();
             for (int i=0; i<quota; i++) {
                 try {
-                    stack.pop();
-                    consumedCount.getAndIncrement();
-                } catch (NoSuchElementException e) {
-                    System.out.println("tried to pop when empty");
+                    conLock.lock();
+                    try {
+                        var obj = stack.pop();
+                        metrics.addToConsumedData(obj);
+                    } finally {
+                        conLock.unlock();
+                    }
+                    metrics.incrementConsumedCount();
                 } catch (InterruptedException e) {
                     System.out.println(e.getMessage());
                 }
             }
             var opsEnd = System.nanoTime();
             var latencyForThread = (opsEnd-opsBegin) / quota;
-            latencyList.add(latencyForThread);
+            metrics.addLatency(latencyForThread);
         };
     }
 
     private static <T> Thread[] getConfiguredThreadPool(
             ThreadType threadType,
-            Configurable testData,
             Stack<T> stack,
-            List<Long> latencyList
+            StackPerformanceMetrics<T> metrics
     ) {
         ArrayList<Thread> threadPool = new ArrayList<>();
         if (threadType == ThreadType.PRODUCER) {
@@ -169,7 +135,7 @@ public class UseStack {
                 int quota = baseForProducers;
                 if (i < extraForProducers) quota += 1;
 
-                threadPool.add(new Thread(getProducerRunnable(quota, stack, latencyList), "Producer "+i));
+                threadPool.add(new Thread(getProducerRunnable(quota, stack, metrics), "Producer "+i));
             }
         }
         if (threadType == ThreadType.CONSUMER) {
@@ -181,7 +147,7 @@ public class UseStack {
                 int quota = baseForConsumers;
                 if (i < extraForConsumers) quota += 1;
 
-                threadPool.add(new Thread(getConsumerRunnable(quota, stack, latencyList), "Consumer "+i));
+                threadPool.add(new Thread(getConsumerRunnable(quota, stack, metrics), "Consumer "+i));
             }
         }
         return threadPool.toArray(new Thread[0]);
