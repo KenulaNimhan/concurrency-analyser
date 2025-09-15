1c7b1a91c22ae868a5397e2065724edddf5a0216
KenulaNimhan <kenulanimhan@gmail.com>
2025-09-15 09:18:35 +0530

    implement test for stacks

---
+++ src/test/java/analyser/structure/stack/SyncStackTest.java
@@ -0,0 +1,80 @@
+package analyser.structure.stack;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class SyncStackTest {
+
+    SyncStack<Integer> underTest = new SyncStack<>(5);
+
+    @Test
+    void shouldGiveCorrectCapacityEvenWhenEmpty() {
+        // assert
+        assertThat(underTest.cap()).isEqualTo(5);
+    }
+
+    @Test
+    void shouldGiveCorrectCapacityWhenContainsElements() throws InterruptedException {
+        // arrange
+        underTest.push(2);
+
+        // assert
+        assertThat(underTest.cap()).isEqualTo(5);
+    }
+
+    @Test
+    void shouldGiveCorrectSizeOfStack() throws InterruptedException {
+        // arrange
+        underTest.push(2);
+        underTest.push(3);
+
+        // assert
+        assertThat(underTest.size()).isEqualTo(2);
+    }
+
+    @Test
+    void shouldIndicateWhenEmpty() {
+        // assert
+        assertThat(underTest.isEmpty()).isTrue();
+    }
+
+    @Test
+    void shouldIndicateWhenNotEmpty() throws InterruptedException {
+        // arrange
+        underTest.push(5);
+        underTest.push(4);
+        underTest.push(2);
+
+        // assert
+        assertThat(underTest.isEmpty()).isFalse();
+    }
+
+    @Test
+    void accountingMethodIsLIFO() throws InterruptedException {
+        // arrange
+        int[] orderToProduce = new int[] {1, 2, 3, 4, 5};
+
+        // populate the stack
+        for (int val: orderToProduce) {
+            underTest.push(val);
+        }
+
+        // de-populate the stack
+        ArrayList<Integer> consumedOrder = new ArrayList<>(5);
+        while (!underTest.isEmpty()) {
+            consumedOrder.addLast(underTest.pop());
+        }
+
+        // assert
+        for (int i=0; i<orderToProduce.length; i++) {
+            assertThat(orderToProduce[i]).isEqualTo(consumedOrder.reversed().get(i));
+        }
+    }
+}
