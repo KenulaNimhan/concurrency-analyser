3a4299e15cdd91b00169ae6d034a6cfb56a93226
KenulaNimhan <kenulanimhan@gmail.com>
2025-09-15 12:35:08 +0530

    tests for atomic stack

---
+++ src/test/java/analyser/structure/core/NodeBasedStackTest.java
@@ -0,0 +1,87 @@
+package analyser.structure.core;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.NoSuchElementException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+class NodeBasedStackTest {
+
+    NodeBasedStack<Integer> underTest = new NodeBasedStack<>(5);
+
+    @Test
+    void shouldGiveCorrectSizeOfStack() {
+        // arrange
+        underTest.push(2);
+        underTest.push(3);
+
+        // assert
+        assertThat(underTest.size()).isEqualTo(2);
+    }
+
+    @Test
+    void shouldThrowException_TryToPush_WhenFull() {
+        // arrange
+        for (int i=0; i<5; i++) {
+            underTest.push(i);
+        }
+
+        // assert
+        assertThatThrownBy(() -> underTest.push(1))
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessageContaining("stack is full");
+
+    }
+
+    @Test
+    void shouldThrowException_TryToPop_WhenEmpty() {
+        // assert
+        assertThatThrownBy(() -> underTest.pop())
+                .isInstanceOf(NoSuchElementException.class)
+                .hasMessageContaining("stack is empty");
+    }
+
+    @Test
+    void shouldIndicateWhenEmpty() {
+        // assert
+        assertThat(underTest.isEmpty()).isTrue();
+    }
+
+    @Test
+    void shouldIndicateWhenNotEmpty() {
+        // arrange
+        underTest.push(5);
+        underTest.push(4);
+        underTest.push(2);
+
+        // assert
+        assertThat(underTest.isEmpty()).isFalse();
+    }
+
+    @Test
+    void accountingMethodIsLIFO() {
+        // arrange
+        int[] orderToProduce = new int[] {1, 2, 3, 4, 5};
+
+        // populate the stack
+        for (int val: orderToProduce) {
+            underTest.push(val);
+        }
+
+        // de-populate the stack
+        ArrayList<Integer> consumedOrder = new ArrayList<>(5);
+        while (!underTest.isEmpty()) {
+            consumedOrder.addLast(underTest.pop());
+        }
+
+        // assert
+        for (int i=0; i<orderToProduce.length; i++) {
+            assertThat(orderToProduce[i]).isEqualTo(consumedOrder.reversed().get(i));
+        }
+
+    }
+}
---
+++ src/test/java/analyser/structure/stack/TreiberStackTest.java
@@ -0,0 +1,88 @@
+package analyser.structure.stack;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.NoSuchElementException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+class TreiberStackTest {
+
+    TreiberStack<Integer> underTest = new TreiberStack<>(5);
+
+    @Test
+    void shouldGiveCorrectSizeOfStack() throws InterruptedException {
+        // arrange
+        underTest.push(2);
+        underTest.push(3);
+
+        // assert
+        assertThat(underTest.size()).isEqualTo(2);
+    }
+
+    @Test
+    @Disabled
+    // treiber stack is semaphore based
+    void shouldThrowException_TryToPush_WhenFull() throws InterruptedException {
+        // arrange
+        for (int i=0; i<5; i++) {
+            underTest.push(i);
+        }
+
+        // assert
+        assertThatThrownBy(() -> underTest.push(1))
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessageContaining("stack is full");
+
+    }
+
+    @Test
+    void shouldGiveNull_WhenTryToPop_WhenEmpty() {
+        // assert
+        assertThat(underTest.pop()).isNull();
+    }
+
+    @Test
+    void shouldIndicateWhenEmpty() {
+        // assert
+        assertThat(underTest.isEmpty()).isTrue();
+    }
+
+    @Test
+    void shouldIndicateWhenNotEmpty() throws InterruptedException {
+        // arrange
+        underTest.push(5);
+        underTest.push(4);
+        underTest.push(2);
+
+        // assert
+        assertThat(underTest.isEmpty()).isFalse();
+    }
+
+    @Test
+    void accountingMethodIsLIFO() throws InterruptedException {
+        // arrange
+        int[] orderToProduce = new int[] {1, 2, 3, 4, 5};
+
+        // populate the stack
+        for (int val: orderToProduce) {
+            underTest.push(val);
+        }
+
+        // de-populate the stack
+        ArrayList<Integer> consumedOrder = new ArrayList<>(5);
+        while (!underTest.isEmpty()) {
+            consumedOrder.addLast(underTest.pop());
+        }
+
+        // assert
+        for (int i=0; i<orderToProduce.length; i++) {
+            assertThat(orderToProduce[i]).isEqualTo(consumedOrder.reversed().get(i));
+        }
+
+    }
+}
