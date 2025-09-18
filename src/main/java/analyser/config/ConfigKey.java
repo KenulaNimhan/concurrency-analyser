b8055304f4d6e0028f6f24586541a58d3d19f54d
KenulaNimhan <kenulanimhan@gmail.com>
2025-09-18 16:13:25 +0530

    change configurable ranges

--- src/main/java/analyser/config/ConfigKey.java
+++ src/main/java/analyser/config/ConfigKey.java
@@ -2,15 +2,15 @@ package analyser.config;
 
 public enum ConfigKey {
     TO_PRODUCE("how many items needed to be produced & consumed",
-            new int[]{1, 2_500_000}),
+            new int[]{1, 1_000_000}),
     PRODUCER_COUNT("how many producer threads",
-            new int[]{1, 5000}),
+            new int[]{1, 100}),
     CONSUMER_COUNT("how many consumer threads",
-            new int[]{1, 5000}),
+            new int[]{1, 100}),
     CAP("max no. of elements the data structure can hold",
-            new int[]{1, 2_500_000}),
+            new int[]{1, 100_000}),
     ELEMENT_SIZE("size per element (in bytes)",
-            new int[]{1, 2048}),
+            new int[]{1, 512}),
     OPERATIONAL_SCALE("estimated CPU usage for each operation (0 - 10 scale)",
             new int[]{0, 10});
 
