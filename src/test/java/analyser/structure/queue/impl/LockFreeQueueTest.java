d0dac554caf1ceeb90fd2b18d4154948b70941ce
KenulaNimhan <kenulanimhan@gmail.com>
2025-09-18 13:38:51 +0530

    test lock free queue

---
+++ src/test/java/analyser/structure/queue/impl/LockFreeQueueTest.java
@@ -0,0 +1,79 @@
+package analyser.structure.queue.impl;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class LockFreeQueueTest {
+
+    LockFreeQueue<Integer> underTest = new LockFreeQueue<>(5);
+
+    @Test
+    void shouldGiveCorrectCapacityEvenWhenEmpty() {
+        // assert
+        assertThat(underTest.cap()).isEqualTo(5);
+    }
+
+    @Test
+    void shouldGiveCorrectCapacityWhenContainsElements() throws InterruptedException {
+        // arrange
+        underTest.enqueue(2);
+
+        // assert
+        assertThat(underTest.cap()).isEqualTo(5);
+    }
+
+    @Test
+    void shouldGiveCorrectSizeOfQueue() throws InterruptedException {
+        // arrange
+        underTest.enqueue(2);
+        underTest.enqueue(3);
+
+        // assert
+        assertThat(underTest.size()).isEqualTo(2);
+    }
+
+
+    @Test
+    void shouldIndicateWhenEmpty() {
+        // assert
+        assertThat(underTest.isEmpty()).isTrue();
+    }
+
+    @Test
+    void shouldIndicateWhenNotEmpty() throws InterruptedException {
+        // arrange
+        underTest.enqueue(5);
+        underTest.enqueue(4);
+        underTest.enqueue(2);
+
+        // assert
+        assertThat(underTest.isEmpty()).isFalse();
+    }
+
+    @Test
+    void accountingMethodIsFIFO() throws InterruptedException {
+        // arrange
+        int[] orderToProduce = new int[] {1, 2, 3, 4, 5};
+
+        // populate the stack
+        for (int val: orderToProduce) {
+            underTest.enqueue(val);
+        }
+
+        // de-populate the stack
+        ArrayList<Integer> consumedOrder = new ArrayList<>(5);
+        while (!underTest.isEmpty()) {
+            consumedOrder.addLast(underTest.dequeue());
+        }
+
+        // assert
+        for (int i=0; i<orderToProduce.length; i++) {
+            assertThat(orderToProduce[i]).isEqualTo(consumedOrder.get(i));
+        }
+
+    }
+
+}
